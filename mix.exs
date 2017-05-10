affd2adefc2d97620cf100ad50d2f9892ae98cea
Mike Buhot <michael.buhot@everydayhero.com.au>
2017-05-10 20:47:27 +1000

    Add distillery releases

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Scout.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:distillery, "~> 1.0"},
+     {:phoenix, "~> 1.3.0-rc"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
