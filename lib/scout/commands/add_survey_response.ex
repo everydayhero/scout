11e1009f554710142ef6b4e49a6d4320c53d307c
Matt Furness <matthew.furness@everydayhero.com>
2017-07-31 21:01:36 +1000

    Use command macro in AddSurveyResponse

--- lib/scout/commands/add_survey_response.ex
+++ lib/scout/commands/add_survey_response.ex
@@ -1,37 +1,14 @@
 defmodule Scout.Commands.AddSurveyResponse do
-  use Ecto.Schema
+  use Scout.Commands.Command
 
-  alias Ecto.{Changeset, Multi}
   alias Scout.Commands.AddSurveyResponse
   alias Scout.Util.ValidationHelpers
   alias Scout.{Response, Survey}
 
-  @primary_key false
-  embedded_schema do
-    field :survey_id, :binary_id
-    field :respondant_email, :string
-    field :answers, {:array, :string}
-  end
-
-  @doc """
-  Casts and validates params to build an `AddSurveyResponse` struct.
-
-  Returns {:ok, %AddSurveyResponse{}} on success, {:error, %Changeset{}} otherwise.
-  """
-  def new(params) do
-    with cs = %{valid?: true} <- validate(params) do
-      {:ok, Changeset.apply_changes(cs)}
-    else
-      changeset -> {:error, changeset}
-    end
-  end
-
-  defp validate(params) do
-    %AddSurveyResponse{}
-    |> Changeset.cast(params, [:survey_id, :respondant_email, :answers])
-    |> Changeset.validate_required([:survey_id, :respondant_email, :answers])
-    |> Changeset.validate_change(:survey_id, &ValidationHelpers.validate_uuid/2)
-    |> Changeset.validate_format(:respondant_email, ~r/@/)
+  cmd do
+    attr :survey_id, :binary_id, [:required, {:custom, &ValidationHelpers.validate_uuid/2}]
+    attr :respondant_email, :string, [:required, {:format, ~r/@/}]
+    attr :answers, {:array, :string}, [:required]
   end
 
   @doc """
