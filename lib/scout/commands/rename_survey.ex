d6413e5de1abc59e95670cc7229c0ded69e1e878
Matt Furness <matthew.furness@everydayhero.com>
2017-09-15 12:54:37 +1000

    Use command macro for RenameSurvey

--- lib/scout/commands/rename_survey.ex
+++ lib/scout/commands/rename_survey.ex
@@ -3,41 +3,15 @@ defmodule Scout.Commands.RenameSurvey do
   Defines the schema and validations for a RenameSurvey command.
   """
 
-  use Ecto.Schema
+  use Scout.Commands.Command
 
-  alias Ecto.Changeset
   alias Scout.Commands.RenameSurvey
   alias Scout.Util.{ErrorHelpers, ValidationHelpers}
   alias Scout.{Repo, Survey, SurveyQuery}
 
-  @primary_key false
-  embedded_schema do
-    field :id, :binary_id
-    field :name, :string
-  end
-
-  @doc """
-  Create a new RenameSurvey struct from string-keyed map of params
-
-  If validations fails, result is `{:error, errors}`, otherwise returns {:ok, struct}
-   - "id" is required and must b a uuid
-   - "name" is required
-
-  returns {:error, errors} on validation failure, {:ok, struct} otherwise.
-  """
-  def new(params) do
-    with cs = %{valid?: true} <- validate(params) do
-      {:ok, Changeset.apply_changes(cs)}
-    else
-      changeset -> {:error, changeset}
-    end
-  end
-
-  defp validate(params) do
-    %RenameSurvey{}
-    |> Changeset.cast(params, [:id, :name])
-    |> Changeset.validate_required([:id, :name])
-    |> Changeset.validate_change(:id, &ValidationHelpers.validate_uuid/2)
+  command do
+    attr :id, :binary_id, required: true, validate: &ValidationHelpers.validate_uuid/2
+    attr :name, :string, required: true
   end
 
   @doc """
