cbbaf65c22e484772af1724367d738599db39928
Matt Furness <matthew.furness@everydayhero.com>
2017-09-16 16:10:48 +1000

    Move EmbeddedQuestion to a child module

--- lib/scout/commands/create_survey.ex
+++ lib/scout/commands/create_survey.ex
@@ -3,18 +3,42 @@ defmodule Scout.Commands.CreateSurvey do
   Defines the schema and validations for the parameters required to create a new survey.
   Note that this doesn't define the database schema, only the structure of the external params payload.
   """
+  defmodule Question do
+    use Scout.Commands.Command
+
+    command do
+      attr :question, :string, required: true
+      attr :answer_format, :string, required: true
+      attr :options, {:array, :string}
+
+      validate &validate_options/1
+    end
+
+    def validate_options(cs = %Changeset{}) do
+      case Changeset.get_field(cs, :answer_format) do
+        "check" ->
+          cs
+          |> Changeset.validate_required(:options)
+          |> Changeset.validate_length(:options, min: 1)
+        fmt when fmt in ["select", "radio"] ->
+          cs
+          |> Changeset.validate_required(:options)
+          |> Changeset.validate_length(:options, min: 2)
+        _ -> cs
+      end
+    end
+  end
 
   use Scout.Commands.Command
 
   alias Ecto.Multi
-  alias Scout.Commands.{CreateSurvey, EmbeddedQuestion}
   alias Scout.Util.ValidationHelpers
   alias Scout.Survey
 
   command do
     attr :owner_id, :string, required: true, validate: &ValidationHelpers.validate_uuid/2
     attr :name, :string, required: true
-    many :questions, EmbeddedQuestion, required: true
+    many :questions, Question, required: true
   end
 
   @doc """
@@ -23,7 +47,7 @@ defmodule Scout.Commands.CreateSurvey do
   Returns an Ecto.Multi representing the operation/s that must happen to create a new Survey.
   The multi should be run by the callng code using Repo.transaction or merged into a larger Multi as needed.
   """
-  def run(cmd = %CreateSurvey{}) do
+  def run(cmd = %__MODULE__{}) do
     Multi.new()
     |> Multi.insert(:survey, Survey.insert_changeset(cmd))
   end
